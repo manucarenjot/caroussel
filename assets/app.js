ab14f536e2df80d002e41c058d65c8e23f5536a1
manucarenjot <carenjotje@gmail.com>
2021-12-13 11:20:43 +0100

    Caroussel

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,12 @@
 let photo = document.getElementById('photo');
 let arrowLeft = document.getElementById('arrowLeft');
 let arrowRight = document.getElementById('arrowRight');
+let carousel = document.getElementById('caroussel');
+
+let largeur = document.getElementById('largeur');
+let hauteur = document.getElementById('hauteur');
+let send = document.getElementById('send');
+
 
 let lesPhotos = ["url(assets/img/20456790.jpg)", "url(assets/img/tree-736885__480.jpg)", "url(assets/img/20494859.jpg)",
     "url(assets/img/centre-img-1-950x500.jpg)", "url(assets/img/libellule-feuille-l760-h550.jpg)", "url(assets/img/jocelyn-armel-10.15-2015-1.jpg)"]
@@ -28,7 +34,6 @@ function right() {
             photo.style.backgroundImage = lesPhotos[5];
             nombrePhoto++
         }
-        console.log(nombrePhoto);
 
 }
 
@@ -53,7 +58,14 @@ function left() {
         photo.style.backgroundImage = lesPhotos[4];
         nombrePhoto--
     }
-    console.log(nombrePhoto);
+}
+
+
+function modifTaille() {
+    if (largeur.value > '350' && hauteur.value > '350') {
+        carousel.style.width = largeur.value + 'px';
+        carousel.style.height = hauteur.value + 'px';
+    }
 }
 
 arrowLeft.addEventListener("click", () => {
@@ -64,4 +76,8 @@ arrowLeft.addEventListener("click", () => {
 arrowRight.addEventListener("click", () => {
     right();
 
+})
+
+send.addEventListener("click", () => {
+    modifTaille()
 })
