777aa610574ea8c5759b705667e0992dc9769eeb
manucarenjot <carenjotje@gmail.com>
2021-12-13 10:50:29 +0100

    Caroussel

--- assets/app.js
+++ assets/app.js
@@ -2,17 +2,25 @@ let photo = document.getElementById('photo');
 let arrowLeft = document.getElementById('arrowLeft');
 let arrowRight = document.getElementById('arrowRight');
 
-let lesPhotos = ["url(img/20494859.jpg)", "url(assets/img/tree-736885__480.jpg)", "url(img/20494859.jpg)"]
+let lesPhotos = ["url(assets/img/20456790.jpg)", "url(assets/img/tree-736885__480.jpg)", "url(assets/img/20494859.jpg)", "url(assets/img/20456790.jpg)"]
+
+let nombrePhoto = 0;
 
 function right() {
-    for (let i = 0; i<lesPhotos.length; i++) {
-        if (lesPhotos[1]) {
+        if (nombrePhoto === 0) {
             photo.style.backgroundImage = lesPhotos[1];
+            nombrePhoto++
         }
-    }
+        else if (nombrePhoto === 1) {
+            photo.style.backgroundImage = lesPhotos[2];
+            nombrePhoto++
+        }
+        console.log(nombrePhoto);
+
 }
 
 
 arrowRight.addEventListener("click", () => {
     right();
+
 })
